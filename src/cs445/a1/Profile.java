c89615b72acf40ae20773e8de60962957145ed5c
Bunun <zdb11@pitt.edu>
2017-05-28 18:57:46 -0400

    Continuing the assignment

--- src/cs445/a1/Profile.java
+++ src/cs445/a1/Profile.java
@@ -3,5 +3,122 @@ package cs445.a1;
 /**
  * Created by bunbunbunbunbun on 17-05-26.
  */
-public class Profile {
+public class Profile implements ProfileInterface{
+
+    /**
+     * Sets this profile's name.
+     * <p>
+     * <p> If newName is not null, then setName modifies this profile so that
+     * its name is newName. If newName is null, then setName throws
+     * IllegalArgumentException without modifying the profile, for example:
+     * <p>
+     * <p> {@code throw new IllegalArgumentException("Name cannot be null")}
+     *
+     * @param newName The new name
+     * @throws IllegalArgumentException If newName is null
+     */
+    public void setName(String newName) throws IllegalArgumentException {
+
+    }
+
+    /**
+     * Gets this profile's name.
+     *
+     * @return The name
+     */
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * Sets this profile's "about me" blurb.
+     * <p>
+     * <p> If newAbout is not null, then setAbout modifies this profile so that
+     * its about blurb is newAbout. If newAbout is null, then setAbout throws
+     * IllegalArgumentException without modifying the profile.
+     *
+     * @param newAbout The new blurb
+     * @throws IllegalArgumentException If newAbout is null
+     */
+    public void setAbout(String newAbout) throws IllegalArgumentException {
+
+    }
+
+    /**
+     * Gets this profile's "about me" blurb
+     *
+     * @return The blurb
+     */
+    public String getAbout() {
+        return null;
+    }
+
+    /**
+     * Adds another profile to this profile's following set.
+     * <p>
+     * <p> If this profile's following set is at capacity, or if other is null,
+     * then follow returns false without modifying the profile. Otherwise, other
+     * is added to this profile's following set and follow returns true. If this
+     * profile already followed other, then follow returns true even though no
+     * changes were needed.
+     *
+     * @param other The profile to follow
+     * @return True if successful, false otherwise
+     */
+    public boolean follow(ProfileInterface other) {
+        return false;
+    }
+
+    /**
+     * Removes the specified profile from this profile's following set.
+     * <p>
+     * <p> If this profile's following set does not contain other, or if other
+     * is null, then unfollow returns false without modifying the profile.
+     * Otherwise, this profile in modified in such a way that other is removed
+     * from this profile's following set.
+     *
+     * @param other The profile to follow
+     * @return True if successful, false otherwise
+     */
+    public boolean unfollow(ProfileInterface other) {
+        return false;
+    }
+
+    /**
+     * Returns a preview of this profile's following set.
+     * <p>
+     * <p> The howMany parameter is a maximum desired size. The returned array
+     * may be less than the requested size if this profile is following fewer
+     * than howMany other profiles. Clients of this method must be careful to
+     * check the size of the returned array to avoid
+     * ArrayIndexOutOfBoundsException.
+     * <p>
+     * <p> Specifically, following returns an array of size min(howMany, [number
+     * of profiles that this profile is following]). This array is populated
+     * with arbitrary profiles that this profile follows.
+     *
+     * @param howMany The maximum number of profiles to return
+     * @return An array of size &le;howMany, containing profiles that this
+     * profile follows
+     */
+    public ProfileInterface[] following(int howMany) {
+        return new ProfileInterface[0];
+    }
+
+    /**
+     * Recommends a profile for this profile to follow. This returns a profile
+     * followed by one of this profile's followed profiles. Should not recommend
+     * this profile to follow someone they already follow, and should not
+     * recommend to follow oneself.
+     * <p>
+     * <p> For example, if this profile is Alex, and Alex follows Bart, and Bart
+     * follows Crissy, this method might return Crissy.
+     *
+     * @return The profile to suggest, or null if no suitable profile is found
+     * (only if all of this profile's followees' followees are already followed
+     * or this profile itself).
+     */
+    public ProfileInterface recommend() {
+        return null;
+    }
 }
--- src/cs445/a1/Set.java
+++ src/cs445/a1/Set.java
@@ -3,55 +3,165 @@ package cs445.a1;
 /**
  * Created by bunbunbunbunbun on 17-05-26.
  */
-public class Set<E> implements ProfileInterface {
+public class Set<E> implements SetInterface {
 
-    private E[] setCapacity;
+    private E[] profileSet;
+    private int size;
+    private boolean isEmpty;
 
 
-    public Set(int setSize){
-        setCapacity = ((E[]) new Object[setSize]);
+    public Set(int setSize) {
+        profileSet = ((E[]) new Object[setSize]);
     }
 
-    public Set(){
+    public Set() {
         // median number of Facebook feirnds, according to http://www.pewresearch.org/fact-tank/2014/02/03/6-new-facts-about-facebook/
-        setCapacity = ((E[]) new Object[200]);
+        profileSet = ((E[]) new Object[200]);
     }
 
     public Set(E[] entries) {
-        int size = entries.length;
-        setCapacity = ((E[]) new Object[size]);
+        size = entries.length;
+        profileSet = ((E[]) new Object[size]);
     }
 
+    /**
+     * Determines the current number of entries in this set.
+     *
+     * @return The integer number of entries currently in this set
+     */
+    public int getCurrentSize() {
+        int totalEntries = 0;
 
-    public void setName(String newName) throws IllegalArgumentException {
-
+        while (true) {
+            if (profileSet[totalEntries] == null) {
+                return totalEntries;
+            } else {
+                totalEntries++;
+            }
+        }
     }
 
-    public String getName() {
-        return null;
+    /**
+     * Determines whether this set is empty.
+     *
+     * @return true if this set is empty; false if not
+     */
+    public boolean isEmpty() {
+        if(profileSet[0] == null){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void setAbout(String newAbout) throws IllegalArgumentException {
+    /**
+     * Adds a new entry to this set, avoiding duplicates.
+     * <p>
+     * <p> If newEntry is not null, this set does not contain newEntry, and this
+     * set has available capacity (if fixed), then add modifies the set so that
+     * it contains newEntry. All other entries remain unmodified. Duplicates are
+     * determined using the .equals() method.
+     * <p>
+     * <p> If newEntry is null, then add throws IllegalArgumentException without
+     * modifying the set. If this set already contains newEntry, then add
+     * returns false without modifying the set. If this set has a capacity
+     * limit, and does not have available capacity, then add throws
+     * SetFullException without modifying the set.
+     *
+     * @param newEntry The object to be added as a new entry
+     * @return true if the addition is successful; false if the item already is
+     * in this set
+     * @throws SetFullException         If this set has a fixed capacity and does not
+     *                                  have the capacity to store an additional entry
+     * @throws IllegalArgumentException If newEntry is null
+     */
+    public boolean add(Object newEntry) throws SetFullException, IllegalArgumentException {
 
-    }
 
-    public String getAbout() {
-        return null;
-    }
 
-    public boolean follow(ProfileInterface other) {
         return false;
     }
 
-    public boolean unfollow(ProfileInterface other) {
+    /**
+     * Removes a specific entry from this set, if possible.
+     * <p>
+     * <p> If this set contains the entry, remove will modify the set so that it
+     * no longer contains entry. All other entries remain unmodified.
+     * Identifying this entry is accomplished using the .equals() method.
+     * <p>
+     * <p> If this set does not contain entry, remove will return false without
+     * modifying the set. If entry is null, then remove throws
+     * IllegalArgumentException without modifying the set.
+     *
+     * @param entry The entry to be removed
+     * @return true if the removal was successful; false if not
+     * @throws IllegalArgumentException If entry is null
+     */
+    public boolean remove(Object entry) throws IllegalArgumentException {
         return false;
     }
 
-    public ProfileInterface[] following(int howMany) {
-        return new ProfileInterface[0];
+    /**
+     * Removes an arbitrary entry from this set, if possible.
+     * <p>
+     * <p> If this set contains at least one entry, remove will modify the set
+     * so that it no longer contains one of its entries. All other entries
+     * remain unmodified. The removed entry will be returned.
+     * <p>
+     * <p> If this set is empty, remove will return null without modifying the
+     * set. Because null cannot be added, a return value of null will never
+     * indicate a successful removal.
+     *
+     * @return The removed entry if the removal was successful; null otherwise
+     */
+    public Object remove() {
+        return null;
     }
 
-    public ProfileInterface recommend() {
-        return null;
+    /**
+     * Removes all entries from this set.
+     * <p>
+     * <p> If this set is already empty, clear will not modify the set.
+     * Otherwise, the set will be modified so that it contains no entries.
+     */
+    public void clear() {
+        for (int i = 0; i < profileSet.length; i++) {
+            profileSet[i] = null;
+            isEmpty = true;
+        }
+    }
+
+    /**
+     * Tests whether this set contains a given entry. Equality is determined
+     * using the .equals() method.
+     * <p>
+     * <p> If this set contains entry, then contains returns true. Otherwise
+     * (including if this set is empty), contains returns false. If entry is
+     * null, then remove throws IllegalArgumentException. The method never
+     * modifies this set.
+     *
+     * @param entry The entry to locate
+     * @return true if this set contains entry; false if not
+     * @throws IllegalArgumentException If entry is null
+     */
+    public boolean contains(Object entry) throws IllegalArgumentException {
+        return false;
+    }
+
+    /**
+     * Retrieves all entries that are in this set.
+     * <p>
+     * <p> An array is returned that contains a reference to each of the entries
+     * in this set. The returned array's length will be equal to the number of
+     * elements in this set, and thus the array will contain no null values.
+     * <p>
+     * <p> If the implementation of set is array-backed, toArray will not return
+     * the private backing array. Instead, a new array will be allocated with
+     * the appropriate capacity.
+     *
+     * @return A newly-allocated array of all the entries in this set
+     */
+    public Object[] toArray() {
+        return new Object[0];
     }
 }
